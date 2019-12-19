fec4136b8cdde553308bca33531820bf05b361b3
Chris Feng <chris.feng@scotiabank.com>
2019-12-18 21:11:33 -0500

    test

---
+++ src/app/main/sample/sample.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { FuseSharedModule } from '@fuse/shared.module';
+
+import { SampleComponent } from './sample.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
+const routes = [
+    {
+        path     : 'sample',
+        component: SampleComponent
+    }
+];
+
+@NgModule({
+    declarations: [
+        SampleComponent
+    ],
+    imports     : [
+        RouterModule.forChild(routes),
+
+        TranslateModule,
+
+        FuseSharedModule,
+        NgZorroAntdModule
+    ],
+    exports     : [
+        SampleComponent
+    ]
+})
+
+export class SampleModule
+{
+}
